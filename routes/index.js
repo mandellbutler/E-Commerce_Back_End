db5b99b41e7be6670f2292e70da420fc5069af0d
Mandell Butler <mandell.butler@outlook.com>
2021-05-22 17:08:56 -0400

    Debugged routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
+// const { Category } = require('../models');
 const apiRoutes = require('./api');
+// const categoryRoutes = require('./api/category-routes');
+// const productRoutes = require('./api/product-routes');
+// const tagRoutes = require('./api/tag-routes');
+
+
+
+// router.use((req, res) => {
+//   res.send("<h1>Wrong Route!</h1>")
+// });
 
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// router.use("/categories", categoryRoutes);
+// router.use("/products", productRoutes);
+// router.use("/tags", tagRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
-//import Models
+// //import Models
 const Category = require('./models/Category');
 const Product = require('./models/Product');
 const ProductTag = require('./models/ProductTag');
@@ -16,13 +16,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+
 // app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}!`);
 // });
 
-sequelize.sync({ force: true }).then(() => {
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
