11894de2cc69fe16e0a3330431961faec0529e1e
Mandell Butler <mandell.butler@outlook.com>
2021-05-21 21:57:13 -0400

    Added product by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,6 +32,33 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+        id: req.params.id
+      },
+      attributes: ["id", "product_name", "price", "stock"],
+      include: [
+        {
+          model: Category,
+          attributes: ["category_name"]
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"]
+        }
+      ]  
+    })
+      .then(productData => {
+        if(!productData) {
+          res.status(404).json({message: "Product ID not found"});
+          return;
+        }
+        res.json(productData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 // create new product
